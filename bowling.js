3f09c587888b88ba7253443174be7483fa39ab12
Braxton Gunter <braxton.gun@gmail.com>
2016-11-16 02:57:50 -0500

    scorecard implementation works as desired! now to universalize the output datatype

--- bowling.js
+++ bowling.js
@@ -1,6 +1,11 @@
 #! /usr/local/bin/node
 // MustWintern Application ~ Braxton
 
+/*
+ * scoreCard data formatting
+ * { roll1: val, roll2: val, *roll3: val, total: val }
+ */
+
 function BowlingGame(pins) {
   this.scoreCard = []; // TODO: store info and print card
   this.rolls = pins;
@@ -27,42 +32,88 @@ function BowlingGame(pins) {
   this.isSpare = function(frame) {
     return this.frameTotal(frame) == 10;
   }
-  // next two rolls
+  // value of next two rolls
   this.strikeBonus = function(frame) {
     return this.frameTotal(frame+1);
   }
-  // next roll
+  // value of next roll
   this.spareBonus = function(frame) {
     return this.rolls[frame+2];
+  }
+  // last round - scorecard 3rd box
+  this.determineThird = function(frame) {
+    if (this.isStrike(frame+2)) return "X";
+    else if (this.isSpare(frame+1)) return "/";
+    else return this.rolls[frame+2];
   }
   // get score
   this.getScore = function() {
     var score = 0;
-    var frame = 0;
+    var frame = 0; // treat as half frame except in case of X
 
+    // 10 rounds by default but until incomplete game
     var frameCount = this.frameCount();
-
     for (var i=0; i<frameCount; i++) {
       if (this.isStrike(frame)) { // strike
         score += 10 + this.strikeBonus(frame);
+        // insert into score card
+        if (i != 9) { // below 10th frame 
+          this.scoreCard.push({ roll1: "X", 
+            roll2: "", 
+            total: typeof(score) != "undefined" ? score : "" 
+          });
+        } else { // 10th frame has 3
+          this.scoreCard.push({ roll1: "X", 
+            roll2: this.rolls[frame+1] == 10 ? "X" : this.rolls[frame+1], 
+            roll3: this.determineThird(frame), 
+            total: typeof(score) != "undefined" ? score : "" 
+          });
+        }
         frame++;
       } else if (this.isSpare(frame)) { // spare
         score += 10 + this.spareBonus(frame);
+        // insert into score card
+        if (i != 9) { // below 10th frame 
+          this.scoreCard.push({ roll1: this.rolls[frame], 
+            roll2: "/", 
+            total: typeof(score) != "undefined" ? score : "" 
+          });
+        } else { // 10th frame has 3
+          this.scoreCard.push({ roll1: this.rolls[frame], 
+            roll2: "/", 
+            roll3: this.determineThird(frame),
+            total: typeof(score) != "undefined" ? score : "" 
+          });
+        }
         frame += 2;
       } else { // open frame
         score += this.frameTotal(frame);
+        // insert into score card
+        if (i != 9) { // below 10th frame
+          this.scoreCard.push({ roll1: this.rolls[frame], 
+            roll2: this.rolls[frame+1], 
+            total: typeof(score) != "undefined" ? score : "" 
+          });
+        } else { // 10th frame has 3
+          this.scoreCard.push({ roll1: this.rolls[frame],
+            roll2: this.rolls[frame+1],
+            roll3: "" // w/o spare || strike 3rd frame is empty
+          }); 
+        }
         frame += 2; 
       } 
     }
-
     return score;
   }
 }
 
+
+// take in input
 var pins = [];
 for (var i=2; i<process.argv.length; i++) {
   pins.push(parseInt(process.argv[i]));
 }
-
+// initialize game
 var game = new BowlingGame(pins);
 console.log(game.getScore());
+console.log(game.scoreCard);
